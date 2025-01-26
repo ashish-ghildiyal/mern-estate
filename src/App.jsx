d04ab8a6d332246386289f84cef107d4560c487d
Ashish <ashish.ghildiyal@xelba.io>
2025-01-26 15:29:54 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import SignOut from './pages/SignOut'
 import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 import Error from './pages/Error'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
     <BrowserRouter>  
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
