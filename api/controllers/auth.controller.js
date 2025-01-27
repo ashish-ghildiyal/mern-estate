f7b634231d76702149aed2566f8883a7e1e8f989
Ashish <ashish.ghildiyal@xelba.io>
2025-01-27 14:48:51 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import { errorHandler } from '../utils/error.js';
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -22,4 +23,39 @@ export const signup = async (req, res, next) => {
         // next(error);      
     }
    
-}   
+} 
+
+
+export const  signin = async(req, res, next)=> {
+    const { email, password } = req.body;
+    try {
+
+    if (!email) {
+      return res.status(400).json({ error: 'Email is required' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Password is required' });
+    }
+
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET)
+    const {password:pass, ...rest} =validUser._doc
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none', 
+    }).status(200).json(rest);
+} catch (error) {
+    next(error);
+}
+  
+    }
